cd3f76933d9e61a75368f0e20ebe06cfd6b80bb1
Calvin Kling <klingcal@gmail.com>
2021-04-10 16:11:40 -0400

    js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-const fightContainer = document.querySelector("#fight-container");
+const fightContainer = document.querySelector("#strength-container");
 
-const fetchFightData = async () => {};
+const fetchFightData = async () => {
+	let response = await fetch("https://www.dnd5eapi.co/api/classes/");
+	let json = await response.json();
+	console.log(json);
+
+	const barbarian = document.createElement("div");
+	const name = document.createElement("h1");
+	name.innerHTML = json.results[0]["name"];
+	console.log(name);
+
+	barbarian.append(name);
+	fightContainer.append(barbarian);
+};
+
+const submit = document.querySelector("button");
+submit.addEventListener("click", function () {
+	fetchFightData();
+});
